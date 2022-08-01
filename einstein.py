bc32e18ceff1c1917a51d635793fea5ede81c815
Joskalitoh <110200037+Joskalitoh@users.noreply.github.com>
2022-08-01 12:50:45 +0300

    Assignment 1

---
+++ einstein.py
@@ -0,0 +1,5 @@
+M=int(input("input the mass  "))
+C=int(input("imput the speed of light  "))
+
+E=M*C**2
+print(E)
---
+++ indoor.py
@@ -0,0 +1,2 @@
+user=input("Please type in what you would like")
+print(user.lower())
